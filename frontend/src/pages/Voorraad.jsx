a621417b270f3e991a836f4d0c9e3799b8fc4df2
Pjotr <pjotrzwartenkot@gmail.com>
2026-01-08 20:30:24 +0100

    voeg testcode toe aan Voorraad.jsx

--- frontend/src/pages/Voorraad.jsx
+++ frontend/src/pages/Voorraad.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Button from "../components/Button";
+import SearchBox from "../components/SearchBox";
 import Separator from "../components/Separator";
 import "./index.css";
 
 const Voorraad = ({ setPage }) => {
+  const [textValue, setTextValue] = useState("");
+
   return (
     <div className="container">
       <h1>Voorraadbeheer</h1>
       <Separator variant="vertical" />
+      <SearchBox
+        size="big"
+        value={textValue}
+        onChange={(e) => setTextValue(e.target.value)}
+      />
       <Button onClick={() => setPage("home")} size="small" label="Terug" />
     </div>
   );
