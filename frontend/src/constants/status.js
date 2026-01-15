d8f5d8723dc2bacd23fd1dd8e1bdd192bcd7c47c
Pjotr <pjotrzwartenkot@gmail.com>
2026-01-15 23:50:21 +0100

    feat: move MUTATIE_TYPE constants to a new file for better organization

--- frontend/src/constants/status.js
+++ frontend/src/constants/status.js
@@ -14,22 +14,3 @@ export const STATUS_ARRAY = Object.values(STATUS).map((s) => STATUS_LABELS[s]);
 
 export const getStatusLabel = (status) =>
   STATUS_LABELS[status] ?? String(status);
-
-export const MUTATIE_TYPE = {
-  LEVERING: 0,
-  VERKOOP: 1,
-  VERLIES: 2,
-  ANDERS: 3,
-};
-
-export const MUTATIE_TYPE_LABELS = {
-  [MUTATIE_TYPE.LEVERING]: "Levering",
-  [MUTATIE_TYPE.VERKOOP]: "Verkoop",
-  [MUTATIE_TYPE.VERLIES]: "Verlies",
-  [MUTATIE_TYPE.ANDERS]: "Anders",
-};
-
-export const MUTATIE_TYPE_ARRAY = Object.values(MUTATIE_TYPE_LABELS);
-
-export const getMutatieTypeLabel = (mutatie_type) =>
-  MUTATIE_TYPE_LABELS[mutatie_type];
