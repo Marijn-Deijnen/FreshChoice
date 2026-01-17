b4db88394c9eda1387bc1aac03e8d68dfcf14eec
Pjotr <pjotrzwartenkot@gmail.com>
2026-01-17 22:27:04 +0100

    update copyDist script to populate backend build with assets

--- frontend/scripts/copyDist.js
+++ frontend/scripts/copyDist.js
@@ -1,12 +1,15 @@
 import fs from "fs";
 
-const source = "dist";
-const target = "../backend/dist";
+const source1 = "dist";
+const source2 = "assets";
+const target1 = "../backend/dist";
+const target2 = "../backend/dist/assets";
 
 // remove old
-if (fs.existsSync(target)) fs.rmSync(target, { recursive: true });
+if (fs.existsSync(target1)) fs.rmSync(target1, { recursive: true });
 
 // copy built dist to backend folder
-fs.cpSync(source, target, { recursive: true });
+fs.cpSync(source1, target1, { recursive: true });
+fs.cpSync(source2, target2, { recursive: true });
 // remove source
-fs.rmSync(source, { recursive: true });
+fs.rmSync(source1, { recursive: true });
