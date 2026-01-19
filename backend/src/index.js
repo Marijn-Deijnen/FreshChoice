476edabaac36fb5e7b580c714182a1b14f4480ca
xiskewer <pjotrzwartenkot@gmail.com>
2026-01-19 20:14:20 +0100

    refactor: remove Product and Mutatie model sync calls in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,10 @@
 import app from "./app.js";
 import { connectToDatabase } from "./utils/db.js";
 import { PORT } from "./utils/config.js";
-import Product from "./models/Product.js";
-import Mutatie from "./models/Mutatie.js";
 import Levering from "./models/Levering.js";
 
 const main = async () => {
   await connectToDatabase();
-  await Product.sync();
-  await Mutatie.sync();
   await Levering.sync();
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
